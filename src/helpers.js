035c2a4c1f4a6e173d88298658586714f4fef747
shjang7 <lucy.sh.jang@gmail.com>
2019-10-24 23:37:42 +0900

    Show first day weather info

--- src/helpers.js
+++ src/helpers.js
@@ -6,33 +6,106 @@ const $search = document.getElementById('search');
 const $weather = document.getElementById('weather');
 const $currWeatherDiv = document.getElementById('weather1');
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const getIconUrl = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
 const getSunTime = (forecast, { first, second }) => {
   const date = new Date(forecast[first][second] * 1000);
   const hours = date.getHours();
   const min = "0" + date.getMinutes();
-  console.log(forecast[first][second], hours, min);
+  // console.log(forecast[first][second], hours, min);
   return { hours, min };
 }
 
-const createCityHTML = (forecast) => {
-  console.log('forecast: ', forecast);
-  const { hours: riseHours, min: riseMin } = getSunTime(forecast, { first: 'city', second: 'sunrise' });
-  console.log('sunrise: ', `${riseHours}:${riseMin.substr(-2)}`);
-  const { hours: setHours, min: setMin } = getSunTime(forecast, { first: 'city', second: 'sunset' });
-  console.log('sunset: ', `${setHours}:${setMin.substr(-2)}`);
+const sunriseTime = (forecast) => {
+  const { hours, min } = getSunTime(forecast, { first: 'city', second: 'sunrise' });
+  return `${hours}:${min.substr(-2)}`;
 }
 
-const createWeatherHTML = (forecast) => {
-  let a = 0;
-  for(let i = 0; i < 8; i++) {
+const sunsetTime = (forecast) => {
+  const { hours, min } = getSunTime(forecast, { first: 'city', second: 'sunset' });
+  return `${hours}:${min.substr(-2)}`;
+}
+
+const drawText = (tag, text) => {
+  const obj = document.createElement(tag);
+  obj.textContent = text;
+  return obj;
+}
+
+const createCityHTML = (forecast) => {
+  const container = document.createElement('div');
+  container.classList.add('col-md-6', 'weather-description');
+  const header = document.createElement('h3');
+  header.textContent = weekDays[(new Date()).getDay()];
+  container.appendChild(header);
+
+  // const currTime = a * 3 + 12;
+  // container.appendChild(drawText('div', currTime));
+  const sunTime = document.createElement('div');
+  sunTime.textContent = `Daytime: ${sunriseTime(forecast)} - ${sunsetTime(forecast)}`;
+  container.appendChild(sunTime);
+  return container;
+}
+
+const getIndex = (forecast, day) => {
+  let index = 0;
+  for(let i = day*8; i < (day+1)*8; i++) {
     if (forecast.list[i].weather[0].icon != '01n') {
-      a = i;
+      index = i;
       break;
     }
   }
-  const currTime = a * 3 + 12;
-  console.log('time: ', currTime);
+  return index;
+}
+
+const drawIconImage = (forecast, i) => {
+  const iconUrl = getIconUrl(forecast.list[i].weather[0].icon);
+  const image = document.createElement('img');
+  image.src = iconUrl;
+  return image;
+}
+
+const minMaxDegree = (forecast, i) => {
+  const maxTemp = forecast.list[i].main.temp_max;
+  const minTemp = forecast.list[i].main.temp_min;
+  return {
+    maxTemp: kelvinToCelsius(maxTemp),
+    minTemp: kelvinToCelsius(minTemp),
+  };
+}
+
+const drawWindDirection = (deg) => {
+  // temp : deg not reflect
+  const direction = document.createElement('i');
+  direction.classList.add('fas', 'fa-location-arrow');
+  return direction;
+}
+
+const drawWind = (forecast, i) => {
+  const container = document.createElement('div');
+
+  container.appendChild(drawWindDirection(forecast.list[i].wind.deg));
+  const windSpeed = forecast.list[i].wind.speed;
+  container.textContent = `${windSpeed} m/s`;
+  return container;
+}
+
+const createWeatherHTML = (forecast) => {
+  const container = document.createElement('div');
+  container.classList.add('col-md-6', 'weather-description');
+
+  const a = getIndex(forecast, 0);
+  container.appendChild(drawIconImage(forecast, a));
+  const { maxTemp, minTemp } = minMaxDegree(forecast, a);
+  container.appendChild(drawText('h2', `${minTemp} / ${maxTemp} °C`));
+  container.appendChild(drawText('div', forecast.list[a].weather[0].main));
+  container.appendChild(drawWind(forecast, a));
+  container.appendChild(drawText('div', forecast.list[a].main.pressure));
+  return container;
+}
+
+
+const createNthDayHtml = (forecast, a) => {
   const windDeg = forecast.list[a].wind.deg;
   console.log('wind deg: ', windDeg);
   const windSpeed = forecast.list[a].wind.speed;
@@ -41,20 +114,18 @@ const createWeatherHTML = (forecast) => {
   console.log('max temp: ', kelvinToCelsius(maxTemp));
   const minTemp = forecast.list[a].main.temp_min;
   console.log('minTemp: ', kelvinToCelsius(minTemp));
-  const condition = forecast.list[a].weather[0].main;
-  console.log('condition: ', condition);
-  const icon = forecast.list[a].weather[0].icon;
-  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  const temp = document.createElement('img');
-  temp.src = iconUrl;
-  $weatherDivAll[0].appendChild(temp);
-  console.log('iconUrl: ', iconUrl);
+  $weatherDivAll[a-1].appendChild(drawIconImage(forecast, a));
   const pressure = forecast.list[a].main.pressure;
 }
 
 export const renderForecast = (forecast) => {
+  // main
   const cityContent = createCityHTML(forecast);
   const weatherContent = createWeatherHTML(forecast);
+  $currWeatherDiv.appendChild(cityContent);
+  $currWeatherDiv.appendChild(weatherContent);
+  // next days
+
 }
 
 export const submit = () => {
@@ -67,10 +138,6 @@ export const getCityInput = () => {
   return value;
 }
 
-export const initialSetHtml = () => {
-  $weather.style.visibility = 'hidden';
-}
-
 export const resetHTML = () => {
   // while ($weather.firstChild) {
   //   $weather.removeChild($weather.firstChild);
@@ -79,6 +146,6 @@ export const resetHTML = () => {
     while(div.firstChild) {
       div.removeChild(div.firstChild);
     }
+    div.style.visibility = 'visible';
   });
-  $weather.style.visibility = 'visible';
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const openWeatherKey = dbConfig.OPEN_WEATHER_KEY;
 const weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 
 const getForecast = async () => {
-  console.log(getCityInput());
-  const tempInput = 'Seoul';
-  const urlToFetch = `${weatherUrl}?q=${tempInput}&APPID=${openWeatherKey}`;
+  // console.log(getCityInput());
+  // const tempInput = 'Seoul';
+  const urlToFetch = `${weatherUrl}?q=${getCityInput()}&APPID=${openWeatherKey}`;
   const response = await fetch(urlToFetch);
   try {
     if (response.ok) {
@@ -24,8 +24,6 @@ const executeSearch = () => {
   resetHTML();
   getForecast().then(forecast => renderForecast(forecast));
 }
-
-initialSetHtml();
 
 submit().addEventListener('keypress', e => {
   if (e.keyCode === 13) {
